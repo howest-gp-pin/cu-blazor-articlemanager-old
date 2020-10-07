1da065f087e545cb2a00686f62af25e905a52fa1
wilschok <william.schokkele@howest.be>
2020-10-07 15:52:34 +0200

    Update formatting

--- ArticleManager.Web/Program.cs
+++ ArticleManager.Web/Program.cs
@@ -5,12 +5,12 @@ namespace ArticleManager.Web
 {
     public class Program
     {
-       public static async Task Main(string[] args)
-    {
+        public static async Task Main(string[] args)
+        {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
             await builder.Build().RunAsync();
-    }
-   
+        }
+
     }
 }
