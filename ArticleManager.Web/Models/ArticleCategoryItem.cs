042ba2470dbbdd6b4229590cdf6debe6956a9b67
wilschok <william.schokkele@howest.be>
2020-10-16 11:22:15 +0200

    Add Crud for categories
    Data still is hardcoded

---
+++ ArticleManager.Web/Models/ArticleCategoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleManager.Web.Models
+{
+    public class ArticleCategoryItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ArticleManager.Web/Services/ICRUDService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace ArticleManager.Web.Services
+{
+    public interface ICRUDService<T, K>
+    {
+        Task<T[]> GetList();
+        Task<K> GetNew();
+        Task<K> Get(int id);
+        Task Create(K item);
+        Task Update(K item);
+        Task Delete(int id);
+    }
+}
