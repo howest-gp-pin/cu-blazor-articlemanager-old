cb4cb6ae9a8aca2936c0bdd9bc5277ae8b4fb3cb
wilschok <william.schokkele@howest.be>
2020-10-16 12:12:45 +0200

    Add Crud for Articles
    Entities still hardcoded

---
+++ ArticleManager.Web/Models/ArticleListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ArticleManager.Web.Models
+{
+    public class ArticleListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+}
