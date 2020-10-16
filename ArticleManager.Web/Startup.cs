2048d241973705e0578919d52de3a508dc1cf6be
wilschok <william.schokkele@howest.be>
2020-10-16 11:41:20 +0200

    Add dependency injection for Blazor Server

--- ArticleManager.Web/Startup.cs
+++ ArticleManager.Web/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArticleManager.Web.Models;
+using ArticleManager.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,6 +20,7 @@ namespace ArticleManager.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddTransient<ICRUDService<ArticleCategoryListItem, ArticleCategoryItem>, ArticleCategoriesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
