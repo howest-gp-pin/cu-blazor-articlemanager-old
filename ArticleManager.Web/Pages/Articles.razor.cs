51477348507c8efe20ea97d757f91a8ad924cee3
Schokkelé William <william.schokkele@howest.be>
2020-11-04 14:07:05 +0100

    Update page components for code separation

---
+++ ArticleManager.Web/Pages/Articles.razor.cs
@@ -0,0 +1,72 @@
+﻿using ArticleManager.Core.Models;
+using ArticleManager.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace ArticleManager.Web.Pages
+{
+    public partial class Articles: ComponentBase
+    {
+        private ArticleListItem[] articleListItems = new ArticleListItem[0];
+        private ArticleItem currentArticle;
+        private string error;
+
+        [Inject]
+        private ICRUDService<ArticleListItem, ArticleItem> service { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ShowList();
+        }
+
+        public async Task ShowList()
+        {
+            articleListItems = await service.GetList();
+            this.currentArticle = null;
+        }
+
+        public async Task AddArticle()
+        {
+            this.currentArticle = await service.GetNew();
+        }
+
+        public async Task EditArticle(ArticleListItem item)
+        {
+            this.currentArticle = await service.Get(item.Id);
+        }
+
+        public async Task SaveArticle(ArticleItem item)
+        {
+            try
+            {
+                if (currentArticle.Id == 0)
+                {
+                    await service.Create(currentArticle);
+                }
+                else
+                {
+                    await service.Update(currentArticle);
+                }
+                await this.ShowList();
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+            }
+        }
+
+        public async Task DeleteArticle(ArticleListItem item)
+        {
+            try
+            {
+                await service.Delete(item.Id);
+                await this.ShowList();
+            }
+            catch (Exception ex)
+            {
+                this.error = ex.Message;
+            }
+        }
+    }
+}
