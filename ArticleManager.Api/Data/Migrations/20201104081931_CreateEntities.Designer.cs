b289a9011f4ad0c8edc4b90f6c7eec1b37584c7c
Schokkelé William <william.schokkele@howest.be>
2020-11-04 09:21:42 +0100

    Add ArticlesController, Migration

--- ArticleManager.Api/Data/Migrations/20201104081931_CreateEntities.Designer.cs
+++ ArticleManager.Api/Data/Migrations/20201104081931_CreateEntities.Designer.cs
@@ -4,14 +4,16 @@ using ArticleManager.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ArticleManager.Api.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201104081931_CreateEntities")]
+    partial class CreateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
